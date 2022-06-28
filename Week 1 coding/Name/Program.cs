bb0881a326b51636f2773d9c73117538a01e9a6b
Harsh4598 <harsh.ht008@gmail.com>
2022-06-28 14:57:09 -0400

    week 1 coding

---
+++ Week 1 coding/Name/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+public class Test
+
+{
+
+    public static void Name(int T, List<string> nameList)
+
+    {
+        List<string> result = new List<string>();
+        string M = "";
+        string W = "";
+
+        for (int i = 0; i < nameList.Count; i++)
+        {
+
+            M = nameList[i].Split(" ")[0];
+            W = nameList[i].Split(" ")[1];
+
+            if (M.Length >= W.Length)
+            {
+                string tempResult = "";
+                for (int j = 0; j < W.Length; j++)
+                {
+
+                    if (M.Contains(W[j]) != true)
+                    {
+                        tempResult = "No";
+                        break;
+                    }
+                }
+                if (tempResult != "No")
+                {
+                    result.Add("Yes");
+                }
+                else
+                {
+                    result.Add("No");
+                }
+            }
+
+            else if (W.Length > M.Length)
+            {
+                string tempResult = "";
+                for (int j = 0; j < M.Length; j++)
+                {
+                    if (W.Contains(M[j]) != true)
+                    {
+                        tempResult = "No";
+                        break;
+                    }
+                }
+                if (tempResult != "No")
+                {
+                    result.Add("Yes");
+                }
+                else
+                {
+                    result.Add("No");
+                }
+            }
+
+        }
+
+        foreach (var item in result)
+        {
+            Console.WriteLine(item);
+        }
+
+    }
+
+
+
+    public static void Main()
+
+    {
+
+        List<string> nameList = new List<string>();
+        int T = 0;
+
+        T = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < T; i++)
+        {
+            nameList.Add(Console.ReadLine());
+
+        }
+
+        Name(T, nameList);
+
+    }
+
+}
---
+++ Week 1 coding/WalkontheAxis/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+public class Test
+
+{
+
+    public static void WalkonAxis(int T, List<int> N)
+
+    {
+        List<int> resultList =new List<int>();
+      
+        foreach (var item in N)
+        {
+        
+              int result =item;
+            for (int i = 0; i <= item; i++)
+            {
+                result+= i;
+                
+            }
+           resultList.Add(result);
+        }
+        Console.WriteLine("=========================");
+        foreach (var item in resultList)
+        {
+            Console.WriteLine(item);
+        }
+
+    }
+
+    public static void Main()
+
+    {
+        List<int> N = new List<int>();
+
+        int T = 0;
+
+        T = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < T; i++)
+        {
+            N.Add(int.Parse(Console.ReadLine()));
+        }
+
+        WalkonAxis(T, N);
+
+    }
+
+}
